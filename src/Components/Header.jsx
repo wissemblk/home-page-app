c2a9e7c91116e274c466cf4939919031334e3880
wissBLK04 <wissembelkhodja@gmail.com>
2024-04-19 01:06:16 +0200

    commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./NavBar";
+
+
+export default function Header() {
+    return (
+        <div className="header-container">
+            <Navbar />
+            <div className="header-img"><img src="/Background image.png" /></div>
+        </div>
+    );
+}
+
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,60 @@
+
+
+export default function Navbar() {
+
+   
+    
+    return ( 
+      
+        <div className='nav-container'>
+      <nav className="Nav">
+        <a href="/" className="Nav-Title">
+          <img src="/logo.png" className="Nav-img" />
+        </a>
+        <ul>
+        <li className="SearchBar">
+            <form action="">
+              <input type="text" name="Q" />
+              <button>
+                <img src="/magnifying-glass.png"  />
+              </button>
+            </form>
+          </li>
+  
+          <li className="abril-fatface-regular">
+            <a href="/">HOME</a>
+          </li>
+          <li className="abril-fatface-regular">
+            
+          <div class="dropdown">
+  <button class="dropbtn">GENRES</button>
+  <div class="dropdown-content">
+    <a href="/">Coding</a>
+    <a href="/">Fiction</a>
+    <a href="/">Historical</a>
+  </div>
+</div>
+            
+          </li>
+          <li className="abril-fatface-regular">
+            <a href="/">ABOUT</a>
+          </li>
+          <li >
+            <a href="/" className="logIn">
+              <img src="/user.png" alt="user image" width={30} height={30}></img>
+              Login
+            </a>
+          </li>
+          <li className="font">
+            <div className="SU">
+              <a href="/" className="signUp">
+                Sign Up for free
+              </a>
+            </div>
+          </li>
+        </ul>
+      </nav>
+      </div>
+     
+    );
+  }
